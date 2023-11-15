cc6a073b8e1377e4c9eacf42d53e2c50cdd72e72
codeph-0bia <codephobia642@gmail.com>
2023-11-15 15:33:08 +0530

    add patch + del routes ✅

--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -6,5 +6,7 @@ const router = express.Router();
 router.get('/', NotesControllers.getNotes);
 router.get('/:noteId', NotesControllers.getNote);
 router.post('/', NotesControllers.createNote);
+router.patch('/:noteId', NotesControllers.updateNote);
+router.delete('/:noteId', NotesControllers.deleteNote);
 
 export default router;
