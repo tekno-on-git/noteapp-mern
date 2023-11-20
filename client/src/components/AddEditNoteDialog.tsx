227fe0a0c0b1d096afac73405c6c281359d8277e
codeph-0bia <codephobia642@gmail.com>
2023-11-21 00:03:58 +0530

    add TextInput Field

--- client/src/components/AddEditNoteDialog.tsx
+++ client/src/components/AddEditNoteDialog.tsx
@@ -3,6 +3,7 @@ import { NoteInput } from '../network/notes_api';
 import { Note } from '../models/note';
 import * as NotesApi from '../network/notes_api';
 import { useForm } from 'react-hook-form';
+import TextInputField from './form/TextInputField';
 
 interface AddEditNoteDialogProps {
   onDismiss: () => void;
@@ -50,28 +51,24 @@ const AddEditNoteDialog = ({
 
       <Modal.Body>
         <Form id="addEditNoteForm" onSubmit={handleSubmit(onSubmit)}>
-          <Form.Group className="mb-3">
-            <Form.Label> TItle</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Title"
-              isInvalid={!!errors.title}
-              {...register('title', { required: 'Required' })}
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.title?.message}
-            </Form.Control.Feedback>
-          </Form.Group>
+          <TextInputField
+            name="title"
+            label="Title"
+            type="text"
+            placeholder="Title"
+            register={register}
+            registerOptions={{ required: 'Required' }}
+            error={errors.title}
+          />
 
-          <Form.Group className="mb-3">
-            <Form.Label>Text</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={5}
-              placeholder="Text"
-              {...register('text')}
-            />
-          </Form.Group>
+          <TextInputField
+            name="text"
+            label="Text"
+            as="textarea"
+            rows={5}
+            placeholder="Text"
+            register={register}
+          />
         </Form>
       </Modal.Body>
 
